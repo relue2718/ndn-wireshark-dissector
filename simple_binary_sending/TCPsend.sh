62d742223f2758225ae00377d6cfbab250a62d5d
Qi Zhao <zhaoq0563@gmail.com>
2015-06-03 02:16:19 -0700

    modify some errors

--- simple_binary_sending/TCPsend.sh
+++ simple_binary_sending/TCPsend.sh
@@ -1,17 +1,18 @@
 #!/bin/sh
-#usage: ./TCPsend.sh file_name
+#usage: ./TCPsend.sh port_number file_name length
 
-if [ -z "$1" ];then
-  echo "Usage: ./TCPsend.sh file_name port_number"
-  exit
-fi
-if [ $# -ne 2 ];then
-  echo "Need both file name and port number!"
-  exit
-fi
+#if [ -z "$1" ];then
+#  echo "Usage: ./TCPsend.sh file_name port_number"
+#  exit
+#fi
+#if [ $# -ne 2 ];then
+#  echo "Need both file name and port number!"
+#  exit
+#fi
 
 echo Sending binary data……
 
-dd if=$1 bs=1000 count=1  > /dev/tcp/127.0.0.1/$2
+dd if=$2 bs=$3 count=1  > /dev/tcp/127.0.0.1/$1
 
 echo End
+
